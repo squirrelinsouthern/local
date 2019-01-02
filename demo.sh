70edbc3c6979081005b39da14b549cd93eb50d9b
squirrelinsouthern <squirrel_000@163.com>
2019-01-02 21:31:04 +0800

    作业修改

--- demo.sh
+++ demo.sh
@@ -2,10 +2,19 @@ if [ -d $1 ]; then
   echo 'error: dir exists'
   exit
 else
-  mkdir $1
-  cd $1
-  mkdir css js
-  touch index.html css/style.css js/main.js
+  mkdir -p $1/css $1/js
+  touch $1/css/style.css $1/js/main.js $1/index.html
+
+  echo -e "<!DOCTYPE>" > $1/index.html
+  echo -e "<title>Hello</title>" >> $1/index.html
+  echo -e "<h1>Hi</h1>" >> $1/index.html
+ 
+  echo -e "h1{color: red;}" > $1/css/style.css
+ 
+  echo -e "var string = \"Hello World\"" > $1/js/main.js
+  echo -e "alert(string)" >> $1/js/main.js
+ 
   echo 'success'
   exit
 fi
+
